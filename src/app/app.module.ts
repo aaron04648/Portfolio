7af317aa702ebb9f1df90825cf4bb656cb446dd7
aaron04648 <aaron.stupf@lernende.bfo-vs.ch>
2024-10-16 12:04:27 +0200

    enhance - project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './Pages/homepage/homepage.component';
+import { AboutMeComponent } from './Pages/about-me/about-me.component';
+import { MyWorksComponent } from './Pages/my-works/my-works.component';
+import { FooterComponent } from './Modules/footer/footer.component';
+import { HeaderComponent } from './Modules/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AboutMeComponent,
+    MyWorksComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
