4cbdd93230bbc2bd278b3a20df6f259dc2e2c0e0
aaron04648 <aaron.stupf@lernende.bfo-vs.ch>
2024-12-15 19:02:34 +0100

    add - layouts

---
+++ src/app/Pages/my-works/layout-extra-info/layout-extra-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-extra-info',
+  templateUrl: './layout-extra-info.component.html',
+  styleUrls: ['./layout-extra-info.component.scss']
+})
+export class LayoutExtraInfoComponent {
+
+}
--- src/app/Pages/my-works/my-works.component.ts
+++ src/app/Pages/my-works/my-works.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-my-works',
   templateUrl: './my-works.component.html',
-  styleUrls: ['./my-works.component.scss']
+  styleUrls: ['./my-works.component.scss'],
 })
 export class MyWorksComponent {
-
+  layout: string = 'print';
+  expandContainer: boolean = false;
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
+  showFullContainer() {
+    this.expandContainer = !this.expandContainer;
+    console.log(this.expandContainer);
+    
+  }
 }
