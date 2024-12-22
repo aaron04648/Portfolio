70ae93501320aacfff6cebcb192942ec2d64fbe5
aaron04648 <aaron.stupf@lernende.bfo-vs.ch>
2024-12-22 18:00:41 +0100

    init - aboutMe

--- src/app/Pages/about-me/about-me.component.ts
+++ src/app/Pages/about-me/about-me.component.ts
@@ -1,10 +1,55 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
+  animations: [
+    trigger('moveCenterToRight', [
+      state('middle', style({ transform: 'translateX(0%)', opacity: 1 })),
+      state('right', style({ transform: 'translateX(100%)', opacity: 0.2 })),
+      transition('middle => right', [animate('0.5s ease-in-out')]),
+    ]),
+    trigger('moveLeftToMiddle', [
+      state('left', style({ transform: 'translateX(-100%)', opacity: 0.2 })),
+      state('middle', style({ transform: 'translateX(0%)', opacity: 1 })),
+      transition('left => middle', [animate('0.5s ease-in-out')]),
+    ]),
+    trigger('moveRightToLeft', [
+      state('right', style({ transform: 'translateX(100%)', opacity: 0.2 })),
+      state('left', style({ transform: 'translateX(0%)', opacity: 0.2 })),
+      transition('right => left', [animate('0.5s ease-in-out')]),
+    ]),
+  ],
 })
 export class AboutMeComponent {
+  slides: string[];
+  i: number;
 
+  constructor() {
+    this.i = 0;
+    this.slides = [
+      'https://ep01.epimg.net/elcomidista/imagenes/2022/10/31/articulo/1667206537_604382_1667230832_noticia_normal.jpg',
+      'https://storage.googleapis.com/css-photos/menu-photos/1d2d5a63-1603-473b-9464-e8fa6787f40b.jpeg',
+      'https://ep01.epimg.net/elcomidista/imagenes/2022/01/11/receta/1641893642_902475_1641893828_noticia_normal.jpg',
+    ];
+  }
+  getSlide() {
+    return this.slides[this.i];
+  }
+
+  getPrev() {
+    this.i < this.slides.length - 1 ? this.i++ : (this.i = 0);
+  }
+
+  getNext() {
+    this.i == 0 ? (this.i = this.slides.length - 1) : this.i--;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,15 @@ import { AboutMeComponent } from './Pages/about-me/about-me.component';
 import { HomepageComponent } from './Pages/homepage/homepage.component';
 import { MyWorksComponent } from './Pages/my-works/my-works.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomerLayoutComponent } from './Pages/about-me/customer-layout/customer-layout.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { LayoutExtraInfoComponent } from './Pages/my-works/layout-extra-info/layout-extra-info.component';
 import { Layout1Component } from './Pages/my-works/layout1/layout1.component';
 import { Layout2Component } from './Pages/my-works/layout2/layout2.component';
 import { Layout3Component } from './Pages/my-works/layout3/layout3.component';
 import { Layout4Component } from './Pages/my-works/layout4/layout4.component';
 import { Layout5Component } from './Pages/my-works/layout5/layout5.component';
-import { LayoutExtraInfoComponent } from './Pages/my-works/layout-extra-info/layout-extra-info.component';
-import { ContactComponent } from './Pages/contact/contact.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,7 +27,7 @@ import { ContactComponent } from './Pages/contact/contact.component';
     MyWorksComponent,
     FooterComponent,
     HeaderComponent,
-    
+
     Layout1Component,
     Layout2Component,
     Layout3Component,
@@ -34,10 +35,12 @@ import { ContactComponent } from './Pages/contact/contact.component';
     Layout5Component,
     LayoutExtraInfoComponent,
     ContactComponent,
+    CustomerLayoutComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     //Materials
     MatIconModule,
   ],
